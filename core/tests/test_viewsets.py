0fb5eeaf395dfb1d7d935e78aa68b5b088cb804e
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2022-06-12 15:05:44 -0300

    criando token jwt e configs + testes

--- core/tests/test_viewsets.py
+++ core/tests/test_viewsets.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 USER_URL = reverse('user-list')
+TOKEN_URL = reverse('token')
 
 class UserTest(APITestCase):
     def setUp(self) -> None:
@@ -23,3 +24,29 @@ class UserTest(APITestCase):
         res = self.client.post(USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_create_token_success(self):
+        User.objects.create_user(username='testuser', password='passwordtest')
+        payload = {'username': 'testuser', 'password': 'passwordtest'}
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn('access', res.data)
+    
+    def test_create_token_invalid(self):
+        User.objects.create_user(username='testuser', password='passwordtest')
+        payload = {'username': 'testuser', 'password': 'passwordtest1'}
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertNotIn('access', res.data)
+    
+    def test_create_token_no_user(self):
+        payload = {'username': 'testuser', 'password': 'passwordtest1'}
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertNotIn('access', res.data)
